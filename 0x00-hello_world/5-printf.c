85ac0b3a792f49b49689f40667f8cea791e53806
Kaywuyep <wuyepkate@gmail.com>
2023-06-15 12:22:37 -0700

    hello world

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - a program that uses printf
+ * Return: 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a 0piece of art,");
+return (0);
+}
