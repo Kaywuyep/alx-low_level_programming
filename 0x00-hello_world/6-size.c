00dcbe35a9bab1386b4f034162b86c2e5b603f15
Kaywuyep <wuyepkate@gmail.com>
2023-06-15 19:53:43 -0700

    hello world

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,10 +5,10 @@
  */
 int main(void)
 {
-printf("size of char: %zu byte(s) \n", sizeof(char));
-printf("size of int: %zu byte(s) \n", sizeof(int));
-printf("size of long int: %zu byte(s) \n", sizeof(long int));
-printf("size of long long int: %zu byte(s) \n", sizeof(long long int));
-printf("size of float: %zu byte(s) \n", sizeof(float));
+printf("size of char: %ld byte(s)\n", sizeof(char));
+printf("size of int: %ld byte(s)\n", sizeof(int));
+printf("size of long int: %ld byte(s)\n", sizeof(long int));
+printf("size of long long int: %ld byte(s)\n", sizeof(long long int));
+printf("size of float: %ld byte(s)\n", sizeof(float));
 return (0);
 }
