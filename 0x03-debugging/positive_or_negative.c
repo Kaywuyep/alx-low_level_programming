98b4b31843217fa2f1304e510e3474e6b583f9e7
Kaywuyep <wuyepkate@gmail.com>
2023-06-23 01:50:50 -0700

    debugging

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,29 +1,22 @@
-#include <time.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
 /**
- *  main - positive_or_negative.c
- *  Return: 0 (success)
+ * positive_or_negative - writes positive or negative number
+ * @i: int type value 
  */
 
-int main(void)
+void positive_or_negative(int i)
 {
-int n;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-/* your code goes there */
-if (n > 0)
+if (i > 0)
 {
-printf("%d is positive\n", n);
+printf("%d is positive\n", i);
 }
-else if (n < 0)
+else if (i < 0)
 {
-printf("%d is negative\n", n);
+printf("%d is negative\n", i);
 }
-else
+else if (i == 0)
 {
-printf("%d is zero\n", n);
+printf("%d is zero\n", i);
 }
-return (0);
 }
