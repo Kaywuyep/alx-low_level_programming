b7f696f62a6768135659ffe242f3d1ea014831f1
Kaywuyep <wuyepkate@gmail.com>
2023-06-30 03:37:15 -0700

    advanced

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -9,20 +9,30 @@
 */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-int a, b, c;
-for (a = 0; r[a] != '\0' && r[a] <= size_r; a++)
-{
-for (b = 0; n1[b] != '\0'; b++)
-{
-for (c = 0; n2[c] != '\0'; c++)
-{
-n1[b] = n2[c];
-b++;
-}
-r[a] = n1[b];
-a++;
-}
-return (r);
-}
+int a, b, c = 0;
+int a1, b1, sum, len1, len2;
+for (len1 = 0; n1[len1] != '\0'; len1++)
+;
+for (len2 = 0; n2[len2] != '\0'; len2++)
+;
+if (size_r <= len1 || size_r <= len2)
 return (0);
+a = len1 - 1;
+b = len2 - 1;
+r[size_r - 1] = '\0';
+while (a >= 0 || b >= 0 || c != 0)
+{
+a1 = a >= 0 ? (n1[a] - '0') : 0;
+b1 = b >= 0 ? (n2[b] - '0') : 0;
+sum = a1 + b1 + c;
+c = sum / 10;
+r[--size_r] = (sum % 10) + '0';
+a--;
+b--;
+}
+if (size_r > 0 && r[size_r - 1] == '\0')
+size_r--;
+for (a = 0; size_r > a; a++)
+r[a] = r[a + (len1 + len2 - size_r)];
+return (r);
 }
