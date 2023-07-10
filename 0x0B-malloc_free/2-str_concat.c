9bb4121558261a7a0c49a6c5d5cc4e09f7dfc383
Kaywuyep <wuyepkate@gmail.com>
2023-07-10 08:35:19 -0700

    malloc and free

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,26 +9,34 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-char *con; /*concatenates*/
-unsigned int i, j, a, b;
-
-if (s1 == NULL)
-s1 = " ";
-if (s2 == NULL)
-s2 = " ";
-for (i = 0; s1[i] != '\0'; i++)
-;
-for (j = 0; s2[j] != '\0'; j++)
-;
-con = (char *)malloc((i * sizeof(*s1)) + (j * sizeof(*s2)) + 1);
+char *con;
+unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
+while (s1 && s1[len1])
+len1++;
+while (s2 && s2[len2])
+len2++;
+con =(char *)malloc(sizeof(char) * (strlen(s1)+ strlen(s2) + 1));
 if (con == NULL)
 return (NULL);
-for (a = 0, b = 0; a < (i + j + 1); a++)
+i = 0;
+j = 0;
+if (s1)
 {
-if (a < i)
-con[a] = s1[a];
-else
-con[a] = s2[b++];
+while (i < len1)
+{
+con[i] = s1[i];
+i++;
 }
+}
+if (s2)
+{
+while (i < (len1 + len2))
+{
+con[i] = s2[j];
+i++;
+j++;
+}
+}
+con[i] = '\0';
 return (con);
 }
