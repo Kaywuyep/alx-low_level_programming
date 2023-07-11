14ef0e9795aab5cf965ba5b756d40d6ed340493c
Kaywuyep <wuyepkate@gmail.com>
2023-07-11 02:11:25 -0700

    malloc and free

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,9 +13,9 @@ char *con; /*concatenates*/
 unsigned int i, j, a, b;
 
 if (s1 == NULL)
-s1 = " ";
+s1 = "";
 if (s2 == NULL)
-s2 = " ";
+s2 = "";
 for (i = 0; s1[i] != '\0'; i++)
 ;
 for (j = 0; s2[j] != '\0'; j++)
