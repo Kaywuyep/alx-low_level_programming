b384a20725bf60dbe4d741e6bf5a9ed0e604012d
Kaywuyep <wuyepkate@gmail.com>
2023-07-27 06:08:04 -0700

    task

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,6 +1,6 @@
 section .data
-    hello_msg db "Hello, Holberton", 0   ; Null-terminated string for the message
-    newline db 10                       ; ASCII code for newline character
+    msg db "Hello, Holberton", 0   ; Null-terminated string for the message
+    newline db "%s", 10, 0                       ; ASCII code for newline character
 
 section .text
     global main                         ; Entry point for the program
@@ -8,10 +8,10 @@ section .text
 extern printf                         ; Declare the external C function
 
 main:
-    push rbp                           ; Set up stack frame (required by ABI)
-    mov rdi, hello_msg                 ; Load the address of the message
-    call printf                        ; Call the printf function for the message
-    mov rdi, newline                   ; Load the address of the newline character
-    call printf                        ; Call the printf function for the newline
-    pop rbp                            ; Restore the original stack pointer
-    ret                                ; Return from the main function
+    mov esi, msg
+	mov edi, newline
+	mov eax, 0
+	call printf
+
+	mov eax, 0
+	ret
