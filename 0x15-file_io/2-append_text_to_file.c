cb3c0e3fe12056011fdcf0b36eb57f470cb71b5e
Kaywuyep <wuyepkate@gmail.com>
2023-09-04 07:47:28 -0700

    tasks

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -26,7 +26,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (!file_exists(filename))
 		return (-1);
 	if (text_content == NULL)
-        return (1); /*Nothing to append, return success*/
+		return (1);/*Nothing to append, return success*/
 
 	fd = open(filename, O_WRONLY | O_APPEND);
 
